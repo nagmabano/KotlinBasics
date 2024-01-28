0f6459af99e6fa514370bf1307f99f7e1ade4948
Nagma Bano <nagma.bano@sap.com>
2024-01-28 17:12:33 +0530

    Variable types in kotlin

---
+++ app/src/main/java/com/example/kotlinbasics/VariableTypes.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinbasics
+
+fun main() {
+    val constantNumber = 10
+    var mutableString = "Hello World!"
+
+    println(constantNumber)
+    println(mutableString)
+
+    mutableString = "My name is Nagma"
+    println(mutableString)
+}
