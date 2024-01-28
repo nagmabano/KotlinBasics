fd81effa4574cd7924de05a024f08bb13bc6b7a1
Nagma Bano <nagma.bano@sap.com>
2024-01-28 18:28:26 +0530

    Floating types in kotlin

---
+++ app/src/main/java/com/example/kotlinbasics/FloatingTypes.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinbasics
+
+fun main() {
+    var circleRatio = 3.14
+    println(circleRatio)
+    circleRatio = 3.1415926535
+    println(circleRatio)
+    circleRatio = 3.1415926535897932
+    println(circleRatio)
+}
