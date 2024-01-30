6a7bfb796a611e2980f5468d334360edec1fae43
Nagma Bano <nagma.bano@sap.com>
2024-01-30 11:19:28 +0530

    checks and cast

--- app/src/main/java/com/example/kotlinbasics/Checks.kt
+++ app/src/main/java/com/example/kotlinbasics/Checks.kt
@@ -1,15 +1,21 @@
 package com.example.kotlinbasics
 
 fun main() {
-    val obj = "Sam"
-    if (obj is String) {
-        println(obj.length)
-    }
-
-    if (obj !is String) { // Same as !(obj is String)
-        println("Not a String")
-    } else {
-        println(obj.length)
-    }
+    val obj = "Samsung"
+//    if (obj is String) {
+//        println(obj.length)
+//    }
+//
+//    if (obj !is String) { // Same as !(obj is String)
+//        println("Not a String")
+//    } else {
+//        println(obj.length)
+//    }
+    demo(obj)
 }
 
+fun demo(x: Any) {
+    if (x is String) {
+        print(x.length) // x is automatically cast to String
+    }
+}
