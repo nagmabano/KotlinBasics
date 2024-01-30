6c5f98719c4e26ff79cc5339fb0df3bd0bdc4708
Nagma Bano <nagma.bano@sap.com>
2024-01-30 14:37:48 +0530

    return to a Labels

--- app/src/main/java/com/example/kotlinbasics/Labels.kt
+++ app/src/main/java/com/example/kotlinbasics/Labels.kt
@@ -1,12 +1,17 @@
 package com.example.kotlinbasics
 
 fun main() {
-    loop@ for(i in 1..100) {
-        for(j in 1.. 100) {
-            if(i*5==j){
-                println(j)
-                break@loop
-            }
-        }
+//    loop@ for(i in 1..100) {
+//        for(j in 1.. 100) {
+//            if(i*5==j){
+//                println(j)
+//                break@loop
+//            }
+//        }
+//    }
+
+    listOf<Int>(1,2,3,4,5).forEach lit@ {
+        if(it == 3) return@lit
+        println(it)
     }
 }
