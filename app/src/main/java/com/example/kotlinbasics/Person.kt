1f020fe2e932c14603050cfd8ee653ff5c27b419
Nagma Bano <nagma.bano@sap.com>
2024-01-31 15:31:07 +0530

    Initializer block

--- app/src/main/java/com/example/kotlinbasics/Person.kt
+++ app/src/main/java/com/example/kotlinbasics/Person.kt
@@ -3,7 +3,19 @@ package com.example.kotlinbasics
 data class Person(var name: String , var age: Int) {
 }
 
+class InitOrderDemo(name: String) {
+    val firstPropery = "First property: $name".also(::println)
+    init {
+        println("First initializer block that prints $name")
+    }
+    val secondProperty = "Second Property: ${name.length}".also(::println)
+    init {
+        println("Second initializer block that prints ${name.length}")
+    }
+}
+
 fun main() {
     val person1 = Person("Jhon Doe", 24).also(::println)
     val person2 = Person("Sam", 54).also(::println)
+    InitOrderDemo("Hana")
 }
