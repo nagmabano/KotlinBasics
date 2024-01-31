3443d7a45b6e6ac826693ff6594eef0efeb9f14e
Nagma Bano <nagma.bano@sap.com>
2024-01-31 14:21:46 +0530

    try catch and finally

--- app/src/main/java/com/example/kotlinbasics/Exceptions.kt
+++ app/src/main/java/com/example/kotlinbasics/Exceptions.kt
@@ -4,8 +4,12 @@ import java.lang.Exception
 
 fun main() {
     try {
+        println("Please enter something")
+        val input = readln()
+        val a: Int? = try { input.toInt() } catch (e: NumberFormatException) { null }
+        println(a)
         throw Exception("Hi there!!")
-    } catch (e: Exception) {
+    }  catch (e: Exception) {
         println(e)
     } finally {
         println("In the finally block!!")
