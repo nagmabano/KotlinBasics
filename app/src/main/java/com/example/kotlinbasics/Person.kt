15f479fcf27ece587f2ffeaa62607fdbb3cdf19f
Nagma Bano <nagma.bano@sap.com>
2024-01-31 15:38:19 +0530

    decleration with default values

--- app/src/main/java/com/example/kotlinbasics/Person.kt
+++ app/src/main/java/com/example/kotlinbasics/Person.kt
@@ -6,13 +6,23 @@ data class Person(var name: String , var age: Int) {
 class InitOrderDemo(name: String) {
     val firstPropery = "First property: $name".also(::println)
     init {
-        println("First initializer block that prints $name")
+        println("First initializer block that prints $name") //Primary constructor parameters can be
+                                                            // used in the initializer blocks.
     }
     val secondProperty = "Second Property: ${name.length}".also(::println)
     init {
         println("Second initializer block that prints ${name.length}")
     }
 }
+
+class Customer(name: String) {
+    val customerKey = name.uppercase() // Primary constructor parameters can also be used in property
+                                        // initializers declared in the class body
+}
+
+// decleration with default values
+class Persons(val firstName: String, val lastName: String, var isEmployed: Boolean = true)
+
 
 fun main() {
     val person1 = Person("Jhon Doe", 24).also(::println)
