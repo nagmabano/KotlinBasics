f9ef68cd84fbacd149e11741174308fc77b65686
Nagma Bano <nagma.bano@sap.com>
2024-02-02 20:12:27 +0530

    class with default property

--- app/src/main/java/com/example/kotlinbasics/Dog.kt
+++ app/src/main/java/com/example/kotlinbasics/Dog.kt
@@ -1,6 +1,6 @@
 package com.example.kotlinbasics
 
-class Dog(val name: String, val breed: String) {
+class Dog(val name: String, val breed: String, var age:Int = 0) {
 
     init {
         bark(name)
--- app/src/main/java/com/example/kotlinbasics/Objects.kt
+++ app/src/main/java/com/example/kotlinbasics/Objects.kt
@@ -4,5 +4,5 @@ fun main() {
 
     // creating an object/instance of class Dog
     val daisy = Dog("Daisy", "dwarf poodle")
-    println("${daisy.name} is a ${daisy.breed}")
+    println("${daisy.name} is a ${daisy.breed} and is ${daisy.age} year old")
 }
