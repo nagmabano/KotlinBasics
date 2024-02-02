30340f760a828395a097f27ad2f0b260a5dd3962
Nagma Bano <nagma.bano@sap.com>
2024-02-02 19:38:31 +0530

    class and object/instance

---
+++ app/src/main/java/com/example/kotlinbasics/Dog.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinbasics
+
+class Dog {
+
+    init {
+        bark()
+    }
+
+    fun bark() {
+        println("Woof woof!!")
+    }
+}
---
+++ app/src/main/java/com/example/kotlinbasics/Objects.kt
@@ -0,0 +1,7 @@
+package com.example.kotlinbasics
+
+fun main() {
+
+    // creating an object/instance of class Dog
+    val daisy = Dog()
+}
