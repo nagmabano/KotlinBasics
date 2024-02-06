08abd47ad04c9e03e5077f7ec46324de6cdded0b
Nagma Bano <nagma.bano@sap.com>
2024-02-06 12:18:19 +0530

    abstract class and functions

---
+++ app/src/main/java/com/example/kotlinbasics/Polygon.kt
@@ -0,0 +1,26 @@
+package com.example.kotlinbasics
+
+abstract class Polygon {
+    // abstract member does not have a implementation in its class
+    abstract fun draw()
+    abstract fun paint()
+}
+
+class Rectangle: Polygon() {
+    override fun draw() {
+        println("Draw Rectangle")
+    }
+
+    override fun paint() {
+        println("Paint Rectangle")
+    }
+}
+
+fun main() {
+    val s = Rectangle()
+    s.draw()
+    s.paint()
+
+
+
+}
