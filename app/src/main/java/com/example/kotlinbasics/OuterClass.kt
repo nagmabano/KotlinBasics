6ffc41946df3c508165672cd0ab0ca3af3d81acf
Nagma Bano <nagma.bano@sap.com>
2024-02-07 11:22:47 +0530

    Outer class and Inner class - Inner class can access outer class propert

---
+++ app/src/main/java/com/example/kotlinbasics/OuterClass.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinbasics
+
+class OuterClass {
+    private var property: String = "Outer Property"
+
+    inner class InnerClass() {
+        fun displayOuterProperty() {
+            // val property: String = "Inner Property"
+            // Inner class has access to public and private properties of outer class
+            println(property)
+            // explicitly referring outer class to resolve the name conflict between inner and outer class
+//            println(this@OuterClass.property)
+        }
+    }
+}
+
+fun main() {
+    val outerInstance = OuterClass()
+    // Each instance of inner class is associated with an instance of outer class
+    val innerInstance = outerInstance.InnerClass()
+    innerInstance.displayOuterProperty()
+}
