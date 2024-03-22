3602d747e0ad8699abab417f5ebcafc95c4b65c3
Nagma Bano <nagma.bano@sap.com>
2024-03-22 15:54:27 +0530

    backing property

--- app/src/main/java/com/example/kotlinbasics/Property.kt
+++ app/src/main/java/com/example/kotlinbasics/Property.kt
@@ -1,6 +1,21 @@
 package com.example.kotlinbasics
 
+import java.lang.AssertionError
+
 class MyClass{
+
+    private var _table: Map<String, Int>? = null
+    var table: Map<String, Int>
+        get() {
+            if (_table == null) {
+                _table = HashMap()
+            }
+            return _table ?: throw AssertionError("Null")
+        }
+        set(value) {
+            _table = value
+        }
+
     var counter = 0
         set(value) {
             if (value >= 0){
@@ -15,5 +30,8 @@ fun main() {
     println(count1.counter)
 
     count1.counter = 5
-    println(count1.counter)
+    count1.table = mapOf("apple" to 4)
+    println(count1.table["apple"])
+
+
 }
