60cfe16f1c3c0c1e5c09cc3429aed9bd849257c5
Nagma Bano <nagma.bano@sap.com>
2024-03-22 16:27:32 +0530

    lateinitializing properties

---
+++ app/src/main/java/com/example/kotlinbasics/MyTest.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinbasics
+
+class MyTest {
+    lateinit var subject: TestSubject
+
+    @SetUp fun setup() {
+        subject = TestSubject()
+    }
+
+    @Test fun test() {
+        subject.method()
+    }
+}
---
+++ app/src/main/java/com/example/kotlinbasics/TestSubject.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinbasics
+
+class TestSubject {
+    fun method() {
+        TODO("Not yet implemented")
+    }
+
+}
