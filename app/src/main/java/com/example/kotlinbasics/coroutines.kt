7fa42b66320b48f8ea4dad1ddf1bf7546e4318ca
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-25 14:01:02 +0530

    extract function refactoring

--- app/src/main/java/com/example/kotlinbasics/coroutines.kt
+++ app/src/main/java/com/example/kotlinbasics/coroutines.kt
@@ -6,8 +6,14 @@ import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
     launch { //coroutine builder
-        delay(1000) // non-blocking delay - suspending function
-        println("World!!")
+        doWorld() // extract function refactoring
     }
     println("Hello ")
+}
+
+suspend fun doWorld() {
+    delay(5000) // Calling a suspend function from within a suspend function. A suspend function can be called either
+                        // through a coroutine or through a suspend function. Here delay is a suspend function
+                        // called from a suspend function in a coroutine scope
+    println("World!")
 }
