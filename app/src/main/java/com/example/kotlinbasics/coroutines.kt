3a5bd21d11c9072ff8e50eff6cdfbe386e93fe49
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-25 14:56:20 +0530

    concurrent coroutines

--- app/src/main/java/com/example/kotlinbasics/coroutines.kt
+++ app/src/main/java/com/example/kotlinbasics/coroutines.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
     doWorld()
+    println("done")
 //    launch { //coroutine builder
 //        doWorld() // extract function refactoring
 //    }
@@ -16,11 +17,16 @@ fun main() = runBlocking {
 
 suspend fun doWorld() = coroutineScope {
     launch {
-        delay(5000) // Calling a suspend function from within a suspend function. A suspend function can be called either
+        delay(5000L) // Calling a suspend function from within a suspend function. A suspend function can be called either
         // through a coroutine or through a suspend function. Here delay is a suspend function
         // called from a suspend function in a coroutine scope
         println("World!")
     }
+    launch {
+        delay(1000L)
+        println("Hello1!")
+    }
+
     println("Hello")
 
 }
