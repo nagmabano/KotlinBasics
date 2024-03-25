fd73e68e38d5ce71182829417a1f564055016926
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-25 14:49:53 +0530

    scope builder

--- app/src/main/java/com/example/kotlinbasics/coroutines.kt
+++ app/src/main/java/com/example/kotlinbasics/coroutines.kt
@@ -1,19 +1,26 @@
 package com.example.kotlinbasics
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-    launch { //coroutine builder
-        doWorld() // extract function refactoring
-    }
-    println("Hello ")
+    doWorld()
+//    launch { //coroutine builder
+//        doWorld() // extract function refactoring
+//    }
+//    println("Hello ")
 }
 
-suspend fun doWorld() {
-    delay(5000) // Calling a suspend function from within a suspend function. A suspend function can be called either
-                        // through a coroutine or through a suspend function. Here delay is a suspend function
-                        // called from a suspend function in a coroutine scope
-    println("World!")
+suspend fun doWorld() = coroutineScope {
+    launch {
+        delay(5000) // Calling a suspend function from within a suspend function. A suspend function can be called either
+        // through a coroutine or through a suspend function. Here delay is a suspend function
+        // called from a suspend function in a coroutine scope
+        println("World!")
+    }
+    println("Hello")
+
 }
