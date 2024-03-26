0eacc23c82aceb033e187c2795aa1c18ce673683
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-26 11:33:34 +0530

    using async builder to perform the same task

--- app/src/main/java/com/example/kotlinbasics/KotlinCoroutines.kt
+++ app/src/main/java/com/example/kotlinbasics/KotlinCoroutines.kt
@@ -1,6 +1,8 @@
 package com.example.kotlinbasics
 
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -9,13 +11,15 @@ import kotlin.concurrent.thread
 fun main() = runBlocking {
         println("Main program starts:  ${Thread.currentThread().name}")
 
-        launch { // create a background thread (worker thread)
+        val job:Job = async { // Thread: main, since nonBlocking runs on the main thread and
+            // launch will run on the local scope so it will also run on the main thread
             println("Fake work starts: ${Thread.currentThread().name}") // Thread T1
             mySuspendFunc(1000) //Pretend doing some work
             println("Fake work finished: ${Thread.currentThread().name}") // Either T1 or any of the thread
         }
 
-        mySuspendFunc(2000)
+        // job.cancel() to cancel a coroutine
+        job.join() // job function will wait for the execution of the job to complete
 
         println("Main program ends:  ${Thread.currentThread().name}")
     }
