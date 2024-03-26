639dc8e01d2d2c2f9d1f3fae0424287ce626823b
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-26 11:38:35 +0530

    async returns a deferred object instead of a job object

--- app/src/main/java/com/example/kotlinbasics/KotlinCoroutines.kt
+++ app/src/main/java/com/example/kotlinbasics/KotlinCoroutines.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinbasics
 
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
@@ -11,15 +12,15 @@ import kotlin.concurrent.thread
 fun main() = runBlocking {
         println("Main program starts:  ${Thread.currentThread().name}")
 
-        val job:Job = async { // Thread: main, since nonBlocking runs on the main thread and
-            // launch will run on the local scope so it will also run on the main thread
+        val jobDeffered: Deferred<Unit> = async { // async returns a deferred object
+            // and not the job object. Deferred is a sub type of Job. It is of generic type.
             println("Fake work starts: ${Thread.currentThread().name}") // Thread T1
             mySuspendFunc(1000) //Pretend doing some work
             println("Fake work finished: ${Thread.currentThread().name}") // Either T1 or any of the thread
         }
 
         // job.cancel() to cancel a coroutine
-        job.join() // job function will wait for the execution of the job to complete
+        jobDeffered.join() // job function will wait for the execution of the job to complete
 
         println("Main program ends:  ${Thread.currentThread().name}")
     }
