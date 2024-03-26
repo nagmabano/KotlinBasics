80cab42a84bc1c3c96dd905becd8295c5d9a9313
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-26 11:40:02 +0530

    To receive the Int returned from async use await.

--- app/src/main/java/com/example/kotlinbasics/KotlinCoroutines.kt
+++ app/src/main/java/com/example/kotlinbasics/KotlinCoroutines.kt
@@ -12,15 +12,18 @@ import kotlin.concurrent.thread
 fun main() = runBlocking {
         println("Main program starts:  ${Thread.currentThread().name}")
 
-        val jobDeffered: Deferred<Unit> = async { // async returns a deferred object
+        val jobDeffered: Deferred<Int> = async { // async returns a deferred object
             // and not the job object. Deferred is a sub type of Job. It is of generic type.
             println("Fake work starts: ${Thread.currentThread().name}") // Thread T1
             mySuspendFunc(1000) //Pretend doing some work
             println("Fake work finished: ${Thread.currentThread().name}") // Either T1 or any of the thread
+            20
         }
 
+        val num:Int = jobDeffered.await()
+
         // job.cancel() to cancel a coroutine
-        jobDeffered.join() // job function will wait for the execution of the job to complete
+//        jobDeffered.join() // job function will wait for the execution of the job to complete
 
         println("Main program ends:  ${Thread.currentThread().name}")
     }
