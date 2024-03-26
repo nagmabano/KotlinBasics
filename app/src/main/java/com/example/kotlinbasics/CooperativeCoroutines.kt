00c1356d7e594cdc252704aa2f5ba980dc587d49
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-26 13:47:08 +0530

    Cancellation Exception handling

--- app/src/main/java/com/example/kotlinbasics/CooperativeCoroutines.kt
+++ app/src/main/java/com/example/kotlinbasics/CooperativeCoroutines.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinbasics
 
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
@@ -8,23 +9,26 @@ import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.yield
+import java.lang.Exception
 
 fun main() = runBlocking {// create a blocking coroutine that runs in the current thread(main)
 
     println("Main program starts: ${Thread.currentThread().name}") // main thread
 
     val job: Job = launch(Dispatchers.Default) {// Thread T1 creates a non blocking coroutine
-        for (i in 0..500) {
-            if (!isActive) {
-                return@launch
+        try {
+            for (i in 0..500) {
+                print("$i.")
+                delay(10)
             }
-            print("$i.")
-            Thread.sleep(1)
+        } catch (e: CancellationException) {
+            println("\nException caught safely: $e ")
+        } finally {
+            println("\nClose resources in finally")
         }
+
     }
-    delay(2) // print few values before we cancel
-//    job.cancel()
-//    job.join() // waits for coroutine to finish
+    delay(200) // print few values before we cancel
 
     job.cancelAndJoin() // replaces cancel and join function
 
