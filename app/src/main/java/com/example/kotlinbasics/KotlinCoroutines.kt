7c88480d3199a7159c714cefd0a3b4deb1d1aac9
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-26 11:16:25 +0530

    removing the GlobalScope will make the thread to run on the main as main is the local scope for the launch func here

--- app/src/main/java/com/example/kotlinbasics/KotlinCoroutines.kt
+++ app/src/main/java/com/example/kotlinbasics/KotlinCoroutines.kt
@@ -6,18 +6,20 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 
-fun main() {
-    println("Main program starts:  ${Thread.currentThread().name}")
+fun main() = runBlocking {
+        println("Main program starts:  ${Thread.currentThread().name}")
 
-    GlobalScope.launch { // create a background thread (worker thread)
-        println("Fake work starts: ${Thread.currentThread().name}") // Thread T1
-        delay(1000) //Pretend doing some work
-        println("Fake work finished: ${Thread.currentThread().name}") // Either T1 or any of the thread
+        launch { // create a background thread (worker thread)
+            println("Fake work starts: ${Thread.currentThread().name}") // Thread T1
+            mySuspendFunc(1000) //Pretend doing some work
+            println("Fake work finished: ${Thread.currentThread().name}") // Either T1 or any of the thread
+        }
+
+        mySuspendFunc(2000)
+
+        println("Main program ends:  ${Thread.currentThread().name}")
     }
 
-    runBlocking {// creates a coroutine that blocks the main thread
-        delay(2000)
-    }
-
-    println("Main program ends:  ${Thread.currentThread().name}")
+suspend fun mySuspendFunc(time: Long) {
+    delay(time)
 }
