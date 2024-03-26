4560925f77537bcdee1a5856e6b81ba7a3e730c9
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-26 14:15:13 +0530

    withTimeoutOrNull function will not throw any exception

--- app/src/main/java/com/example/kotlinbasics/CoroutineTimeouts.kt
+++ app/src/main/java/com/example/kotlinbasics/CoroutineTimeouts.kt
@@ -5,22 +5,17 @@ import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
 import java.lang.Exception
 
 fun main() = runBlocking {
     println("\nMain program starts: ${Thread.currentThread().name}")
 
-    withTimeout(2000) {
-        try {
+    withTimeoutOrNull(2000) {
             for (i in 0..500) {
                 println("$i.")
                 delay(500)
             }
-        } catch (ex: TimeoutCancellationException) {
-            println("Exception handled safely: $ex")
-        } finally {
-            println("Close resources in finally!!")
-        }
 
     }
 
